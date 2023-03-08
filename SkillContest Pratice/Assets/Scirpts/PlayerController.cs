bce514da22284321fa6aabec3836a9e9108a3806
Jeongmin Kim <kjmgi0671@naver.com>
2023-03-08 14:43:04 +0900

    23.03.08 commit

--- SkillContest Pratice/Assets/Scirpts/PlayerController.cs
+++ SkillContest Pratice/Assets/Scirpts/PlayerController.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class PlayerController : MonoBehaviour
 {
+    #region È¸Àü °ª
+    readonly private float MAX_ROTATEX = 30f;
+    readonly private float MIN_ROTATEX = -30f;
+    readonly private float MAX_ROTATEZ = 10f;
+    readonly private float MIN_ROTATEZ = -30f;
+    #endregion
+
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotateSpeed;
+    [SerializeField] private float returnRotateSpeed;
     [SerializeField] private GameObject model;
 
     private Vector3 moveInput;
-    private Vector3 defaultRotation = new Vector3(0, 90, 0);
-
     private float rotateX = 0;
     private float rotateZ = 0;
 
@@ -31,14 +36,15 @@ public class PlayerController : MonoBehaviour
     {
         if (moveInput != Vector3.zero)
         {
-            rotateX = Mathf.Clamp(rotateX + moveInput.x * rotateSpeed * Time.deltaTime, -30, 30);
-            rotateZ += moveInput.y * rotateSpeed * Time.deltaTime;
-            model.transform.eulerAngles = new Vector3(rotateX, model.transform.eulerAngles.y, -rotateZ);
+            rotateX = Mathf.Clamp(rotateX + moveInput.x * rotateSpeed * Time.deltaTime, MIN_ROTATEX, MAX_ROTATEX);
+            rotateZ = Mathf.Clamp(rotateZ + moveInput.y * rotateSpeed * Time.deltaTime, MIN_ROTATEZ, MAX_ROTATEZ);           
         }
         else
         {
-            model.transform.eulerAngles = 
-                Vector3.Lerp(model.transform.eulerAngles, defaultRotation, rotateSpeed * Time.deltaTime);
+            rotateX = Mathf.Lerp(rotateX, 0, returnRotateSpeed * Time.deltaTime);
+            rotateZ = Mathf.Lerp(rotateZ, 0, returnRotateSpeed * Time.deltaTime);
         }
+
+        model.transform.eulerAngles = new Vector3(rotateX, model.transform.eulerAngles.y, -rotateZ);
     }
 }
